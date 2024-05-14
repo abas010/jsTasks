6f334146a6afc88e8bd43b4cbe06d986492f9f85
amirabbas <amirabbasqarakhani@gmail.com>
2024-05-14 00:30:22 -0700

    By changing the first select box, the value of the second select box changes

---
+++ 2-ticet/js/script.js
@@ -0,0 +1,27 @@
+﻿//
+let conSelbox = document.querySelector(".countrySelect");
+let citySelect = document.querySelector(".citySelect");
+
+let conData = {
+  Iran: ["tehran", "qazvin", "karag"],
+  Canada: ["Sdot", "sds", "sasdasd", "sxcs"],
+  US: ["aasda", "plo,p", "uhyuhi"],
+};
+
+function chengeSelcct() {
+  if (conSelbox.value === "Please Select") {
+    citySelect.innerHTML = "";
+    citySelect.innerHTML = "<option>Please Select...</option>";
+  } else {
+    let nameCon = conSelbox.value;
+    let conCityes = conData[nameCon];
+
+    citySelect.innerHTML = "";
+
+    conCityes.forEach(function (a) {
+      citySelect.innerHTML += "<option>" + a + "</option>";
+    });
+  }
+}
+
+conSelbox.addEventListener("change", chengeSelcct);
