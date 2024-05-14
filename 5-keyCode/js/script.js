8058ace8178dbe73ece5658b23a8eea736afde91
amirabbas <amirabbasqarakhani@gmail.com>
2024-05-14 00:44:30 -0700

    By pressing the keyboard key, it displays its information

---
+++ 5-keyCode/js/script.js
@@ -0,0 +1,30 @@
+﻿let $ = document;
+
+let valKey = $.getElementById("key");
+let valLoc = $.getElementById("location");
+let valWhich = $.getElementById("which");
+let valKeycode = $.getElementById("keyCode");
+let valCode = $.getElementById("code");
+
+function informayionKey(e) {
+	e.preventDefault();
+
+	starter.style.display = "none";
+	heading.style.display = "flex";
+	ascii.style.display = "flex";
+	infos.style.display = "flex";
+  
+	let eventkey = e.key;
+	let eventLocation = e.location;
+	let eventWhich = e.keyCode;
+	let eventcode = e.code;
+  
+	valKey.innerHTML = eventkey;
+	valLoc.innerHTML = eventLocation;
+	valWhich.innerHTML = eventWhich;
+	valKeycode.innerHTML = eventWhich;
+	valCode.innerHTML = eventcode;
+}
+
+
+$.body.addEventListener("keydown", informayionKey);
