b457dd9d5878a503772a7b74068dbcadc08d6b00
amirabbas <amirabbasqarakhani@gmail.com>
2024-05-14 00:53:41 -0700

    A context menu appears with right click

---
+++ 7-Custom ContextMenu/js/script.js
@@ -0,0 +1,25 @@
+﻿const $ = document;
+
+let clickBody = $.body;
+let contextmenu = $.getElementById("contextMenu");
+
+function showContextmenu(e) {
+  e.preventDefault();
+  contextmenu.style.display = "block";
+  contextmenu.style.left = e.pageX;
+  contextmenu.style.top = e.pageY;
+}
+
+function hiddenContextmenuClick(e) {
+  contextmenu.style.display = "none";
+}
+
+function hiddenContextmenuESC(e) {
+  if (e.keyCode === 27) {
+    contextmenu.style.display = "none";
+  }
+}
+
+clickBody.addEventListener("contextmenu", showContextmenu);
+clickBody.addEventListener("click", hiddenContextmenuClick);
+clickBody.addEventListener("keydown", hiddenContextmenuESC);
